f75330ac378a4be08ca5e25fc9bb4246ded562f4
tamsant <tamsant@amazon.com>
2023-12-21 17:14:15 +0000

    removed cognito domain

--- infrastructure-ts/bin/serverless-chat.ts
+++ infrastructure-ts/bin/serverless-chat.ts
@@ -66,7 +66,7 @@ const frontendStack = new FrontendStack(app, 'FrontendStack', {
   restApi: restApiStack.restApi,
   websocketApi: websocketApiStack.webSocketApi,
   cognitoUserPoolId: authStack.cognitoUserPoolId,
-  cognitoDomainPrefix: 'blah' // Cognito domain prefix needs to be unique globally. Please fill in your domain prefix.
+  cognitoDomainPrefix: '' // Cognito domain prefix needs to be unique globally. Please fill in your domain prefix.
 });
 frontendStack.addDependency(restApiStack);
 
